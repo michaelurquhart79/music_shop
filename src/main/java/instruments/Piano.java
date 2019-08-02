898233c8619921ffcc30f4f279ac19d6ef13a445
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-02 17:15:42 +0100

    Piano class added which extends Instrument.

--- src/main/java/instruments/Piano.java
+++ src/main/java/instruments/Piano.java
@@ -1,4 +1,26 @@
 package instruments;
 
-public class Piano {
+public class Piano extends Instrument{
+
+    private int numberOfKeys;
+    private boolean electric;
+
+    public Piano(String type, String colour, String material, String make, double purchasePrice, double salePrice, int numberOfKeys, boolean electric) {
+        super(type, colour, material, make, purchasePrice, salePrice);
+        this.numberOfKeys = numberOfKeys;
+        this.electric = electric;
+        this.description = "Piano";
+    }
+
+    public String play(){
+        return "I am a " + this.description + " and I go plink plonk.";
+    }
+
+    public int getNumberOfKeys() {
+        return numberOfKeys;
+    }
+
+    public boolean isElectric() {
+        return electric;
+    }
 }
--- src/test/java/PianoTest.java
+++ src/test/java/PianoTest.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import instruments.Piano;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PianoTest {
+
+    Piano piano;
+
+    @Before
+    public void before() {
+        piano = new Piano("Keyboard", "black", "wood", "Yamaha", 1500.00,
+                2500.00, 94, false);
+    }
+
+    @Test
+    public void hasNumberOfKeys() {
+        assertEquals(94, piano.getNumberOfKeys());
+    }
+
+    @Test
+    public void isElectric() {
+        assertEquals(false, piano.isElectric());
+    }
+
+    @Test
+    public void hasDescription() {
+        assertEquals("Piano", piano.getDescription());
+    }
+
+    @Test
+    public void canPlay() {
+        assertEquals("I am a Piano and I go plink plonk.", piano.play());
+    }
+
+    @Test
+    public void canCalculateMarkup() {
+        assertEquals(1000.00, piano.calculateMarkup(), 0.001);
+    }
 }
