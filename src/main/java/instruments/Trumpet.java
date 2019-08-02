ff8aa39a32d521a9ed681679bf3c55f53619b28c
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-02 17:31:16 +0100

    Trumpet class added which extends Instrument.

---
+++ src/main/java/instruments/Trumpet.java
@@ -0,0 +1,20 @@
+package instruments;
+
+public class Trumpet extends Instrument {
+
+    private int numberOfValves;
+
+    public Trumpet(String type, String colour, String material, String make, double purchasePrice, double salePrice, int numberOfValves) {
+        super(type, colour, material, make, purchasePrice, salePrice);
+        this.numberOfValves = numberOfValves;
+        this.description = "Trumpet";
+    }
+
+    public int getNumberOfValves() {
+        return numberOfValves;
+    }
+
+    public String play() {
+        return "I am a " + this.description + " and I go toot toot.";
+    }
+}
---
+++ src/test/java/TrumpetTest.java
@@ -0,0 +1,37 @@
+import instruments.Trumpet;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TrumpetTest {
+
+    Trumpet trumpet;
+
+    @Before
+    public void before() {
+        trumpet = new Trumpet("Brass", "gold", "Brass", "Bach Stradivarius",
+                1000.00, 1400.00, 3);
+    }
+
+    @Test
+    public void hasNumberOfValves() {
+        assertEquals(3, trumpet.getNumberOfValves());
+    }
+
+    @Test
+    public void hasDescription(){
+        assertEquals("Trumpet", trumpet.getDescription());
+    }
+
+    @Test
+    public void canPlay() {
+        assertEquals("I am a Trumpet and I go toot toot.", trumpet.play());
+    }
+
+    @Test
+    public void canCalculateMarkup() {
+        assertEquals(400.00, trumpet.calculateMarkup(), 0.001);
+    }
+
+}
