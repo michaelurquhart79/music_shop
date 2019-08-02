0738454a49b8c4d7db7a8e12e06d4ee1bf9d19de
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-02 14:43:11 +0100

    Added abstract class Instrument and refactored Guitar to inherit from Instrument.

--- src/main/java/instruments/Guitar.java
+++ src/main/java/instruments/Guitar.java
@@ -1,52 +1,18 @@
 package instruments;
 
-public class Guitar {
+public class Guitar extends Instrument{
 
-    private String type;
-    private String colour;
-    private String material;
-    private String make;
-    private double purchasePrice;
-    private double salePrice;
+
     private int noOfStrings;
     private String handed;
     private String description;
 
-    public Guitar(String type, String colour, String material, String make, double purchasePrice,
-                  double salePrice, int noOfStrings, String handed ) {
-        this.type = type;
-        this.colour = colour;
-        this.material = material;
-        this.make = make;
-        this.purchasePrice = purchasePrice;
-        this.salePrice = salePrice;
+    public Guitar(String type, String colour, String material, String make, double purchasePrice, double salePrice,
+                  int noOfStrings, String handed) {
+        super(type, colour, material, make, purchasePrice, salePrice);
         this.noOfStrings = noOfStrings;
         this.handed = handed;
         this.description = "Guitar";
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getColour() {
-        return colour;
-    }
-
-    public String getMaterial() {
-        return material;
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public double getPurchasePrice() {
-        return purchasePrice;
-    }
-
-    public double getSalePrice() {
-        return salePrice;
     }
 
     public int getNoOfStrings() {
@@ -65,7 +31,4 @@ public class Guitar {
         return "I am a " + this.description + " and I go Strum Strum.";
     }
 
-    public double calculateMarkup() {
-        return this.salePrice - this.purchasePrice;
-    }
 }
---
+++ src/main/java/instruments/Instrument.java
@@ -0,0 +1,54 @@
+package instruments;
+
+public abstract class Instrument {
+    private String type;
+    private String colour;
+    private String material;
+    private String make;
+    private double purchasePrice;
+    private double salePrice;
+    private String description;
+
+    public Instrument(String type, String colour, String material, String make, double purchasePrice, double salePrice) {
+        this.type = type;
+        this.colour = colour;
+        this.material = material;
+        this.make = make;
+        this.purchasePrice = purchasePrice;
+        this.salePrice = salePrice;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double calculateMarkup() {
+        return this.salePrice - this.purchasePrice;
+    }
+
+    public abstract String play();
+}
